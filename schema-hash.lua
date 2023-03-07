0b20cd65c7d1b87bbf895ef9691de9bbbf73c6c9
Luna Nova <git@lunnova.dev>
2023-03-07 06:18:44 -0800

    schema-hash: use api which allows passing chunks separately, avoiding concats

--- schema-hash.lua
+++ schema-hash.lua
@@ -10,28 +10,35 @@ local hashHexChars = hashBytes * 2
 -- generates a random hash
 -- math.randomseed should be called before this is used for the first time
 local function randomHash()
-  return string.sub(hashFunc(tostring(math.random(uint32_max)) .. " " .. tostring(math.random(uint32_max))), 1, hashHexChars)
+  local partialHash = hashFunc()
+  partialHash = partialHash(tostring(math.random(uint32_max)))
+  partialHash = partialHash(tostring(math.random(uint32_max)))
+  return string.sub(partialHash(), 1, hashHexChars)
 end
 
 -- hash of every item in input sequence
 local function hash(t)
-  -- PERF: could change interface in sha2.lua to allow passing multiple buffers in in sequence
-  -- so don't need to concat into one big string here? slight improvement
-  local input = ""
-  for i, v in ipairs(t) do
-    if type(v) == 'table' then
-      error(i, v, "unexpected table in inputs")
-    end
-    input = input .. string.char(i % 256) .. tostring(v)
+  if type(t) ~= "table" or #t == 0 then
+    error("hash(): input should be a list with at least one item")
   end
-  return string.sub(hashFunc(input), 1, hashHexChars)
+  local partialHash = hashFunc()
+  for i = 1, #t do
+    local v = t[i]
+    if type(v) == 'table' then
+      error("hash(): unexpected table in input list") -- unexpected table in bagging area
+    end
+    partialHash = partialHash(tostring(v))
+  end
+  return string.sub(partialHash(), 1, hashHexChars)
 end
 
-if not ... then
+if not ... then -- if ran as main script with no args do selftest
   math.randomseed(os.time())
-  print(hashRandom())
-  local testTable = {"test", 1234, "CAFEBABE"}
-  print(hashSequence(testTable))
+  print("randomHash() =\t\t", randomHash())
+  local testTable = {"test", 1234, "DEADBEEF"}
+  local result = hash(testTable)
+  print("hash({test, 1234, DEADBEEF}) =\t", result)
+  assert(result == "6398e970acf8dc2c", "hash of test table should be 6398e970acf8dc2c")
 end
 
 return {
